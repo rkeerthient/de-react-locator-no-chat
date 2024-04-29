8c25ff71eff3071e71b0e1aa76926dccaa9bf630
rkeerthient <rkeerthi@yext.com>
2024-04-29 15:30:30 +0530

    renamed title

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -22,7 +22,7 @@ export const getHeadConfig: GetHeadConfig<
   TemplateRenderProps
 > = (): HeadConfig => {
   return {
-    title: "Wells Fargo | Search",
+    title: "Locator | Search",
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
     tags: [
